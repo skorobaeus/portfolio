1ce5c3641b9023fc6251151ba3363adef1ae1ea1
Svetlana Korobtseva <wizzzjer@gmail.com>
2025-09-25 04:22:56 +0800

    makes dialogs slideable

--- scripts/slider.js
+++ scripts/slider.js
@@ -14,6 +14,7 @@ sliders.forEach((slider) => {
     if (currentSlide > 0) {
         currentSlide--
         updateSlider()
+        syncDialogButtonsForIndex(currentSlide)
     }
     })
 
@@ -21,6 +22,7 @@ sliders.forEach((slider) => {
     if (currentSlide < slideCount - 1) {
         currentSlide++
         updateSlider()
+        syncDialogButtonsForIndex(currentSlide)
     }
     })
 
@@ -36,5 +38,78 @@ sliders.forEach((slider) => {
         prevButton.setAttribute(inactiveButton, currentSlide === 0)
         nextButton.setAttribute(inactiveButton, currentSlide === slideCount - 1)
     }
+
+    const thumbs = slider.querySelectorAll('.slide img')
+    const dialogByIndex = []
+    thumbs.forEach((img, i) => {
+        const src = img.getAttribute('src')
+        const dlgImg = document.querySelector(`dialog img.fullsize[src="${CSS.escape(src)}"]`)
+        if (dlgImg) {
+            const dlg = dlgImg.closest('dialog')
+            dialogByIndex[i] = dlg
+
+            const dlgPrev = dlg.querySelector('.button-prev')
+            const dlgNext = dlg.querySelector('.button-next')
+
+            if (dlgPrev) {
+                dlgPrev.addEventListener('click', () => {
+                    if (i > 0) {
+                        openDialogAt(i - 1)
+                    }
+                })
+            }
+
+            if (dlgNext) {
+                dlgNext.addEventListener('click', () => {
+                    if (i < slideCount - 1) {
+                        openDialogAt(i + 1)
+                    }
+                })
+            }
+        }
+    })
+
+    function closeAllDialogsInGroup() {
+        dialogByIndex.forEach(d => {
+            if (d && d.open) d.close()
+        })
+    }
+
+    function setDialogArrowsState(dlg, index) {
+        if (!dlg) return
+        const dlgPrev = dlg.querySelector('.button-prev')
+        const dlgNext = dlg.querySelector('.button-next')
+        if (dlgPrev) dlgPrev.setAttribute(inactiveButton, index === 0)
+        if (dlgNext) dlgNext.setAttribute(inactiveButton, index === slideCount - 1)
+    }
+
+    function openDialogAt(index) {
+        const dlg = dialogByIndex[index]
+        if (!dlg) return
+        closeAllDialogsInGroup()
+        dlg.showModal()
+        currentSlide = index
+        updateSlider()
+        setDialogArrowsState(dlg, index)
+    }
+
+    function syncAfterInlineOpen(index) {
+        const dlg = dialogByIndex[index]
+        if (!dlg) return
+        currentSlide = index
+        updateSlider()
+        setDialogArrowsState(dlg, index)
+    }
+
+    thumbs.forEach((img, i) => {
+        img.addEventListener('click', () => {
+            setTimeout(() => {
+                const dlg = dialogByIndex[i]
+                if (dlg && dlg.open) {
+                    syncAfterInlineOpen(i)
+                }
+            }, 0)
+        })
+    })
 
 })
